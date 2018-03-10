361ee7dec5fa234b119fa8e067e7f61e4f5483bf
krishna <krish.rk505@gmail.com>
2018-03-10 14:30:09 +0530

    java Application

---
+++ app.java
@@ -0,0 +1,14 @@
+public class app
+{
+public static void main(String args[])
+{
+     System.out.println("Hello welcome to Jenkins world");
+
+     System.out.println("welcome Jenkins_Home");
+
+    System.out.println("Contionoues integartion and Delivery");
+
+     System.out.println("Building application");
+    }
+}
+
