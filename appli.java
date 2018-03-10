480f5dfa503c1564367d31510d2b8005fcff262f
krishna <krish.rk505@gmail.com>
2018-03-10 14:39:03 +0530

    java apps

---
+++ appli.java
@@ -0,0 +1,10 @@
+public class appli
+{
+public static void main(String args[])
+{
+     System.out.println(" Java world");
+
+     System.out.println("welcome Home");
+    }
+}
+
---
+++ apps.java
@@ -0,0 +1,10 @@
+public class apps
+{
+public static void main(String args[])
+{
+     System.out.println("Hello Sample java");
+
+     System.out.println("welcome");
+    }
+}
+
